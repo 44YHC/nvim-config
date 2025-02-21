e753a21b2708f96685f462ad84dd17b0a790c83a
= <=>
2025-02-21 14:44:17 +0200

    go: gotests which-key keymaps

--- lua/plugins/gotests.lua
+++ lua/plugins/gotests.lua
@@ -2,6 +2,10 @@ return {
   {
     "buoto/gotests-vim",
     -- dependencies = { "nvim-lua/plenary.nvim" }, -- Add dependencies if needed
-    config = function() end,
+    config = function()
+      -- Map :GoTests to <leader>t
+      vim.keymap.set("n", "<leader>tt", ":GoTests<CR>", { desc = "Generate Tests Selected" })
+      vim.keymap.set("n", "<leader>ta", ":GoTestsAll<CR>", { desc = "Generate Tests All" })
+    end,
   },
 }
